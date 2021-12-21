d611dd89a8df35f0a221fdc9641bc1ef2065924d
Ezeugo <e.ugotex@gmail.com>
2021-12-20 22:03:19 -0800

    implemented chat functionality

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
-import Main from "../components/Main";
+import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
   const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
       <Head>
         <title>MetaChat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main />
+
+      <div className="max-w-screen-2xl mx-auto">
+        <Header />
+        <Messages />
+      </div>
     </div>
   );
 }
